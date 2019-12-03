c46ec2b80c60223bbc41c21d89e2ce103375e58f
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-12-03 17:03:30 -0500

    fetching my data and followers data

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Github UserCard App</h1>
-    </div>
-  );
+import axios from "axios";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      myCard: [],
+      followers: []
+    }
+  }
+
+  fetchUser = () => {
+    axios
+    .get("https://api.github.com/users/chelsabeth")
+    .then(res => {
+      this.setState({
+        myCard: res.data})
+    })
+    console.log("My Github Data: ", this.state.myCard);
+  }
+
+  componentDidMount() {
+    this.fetchUser("chelsabeth")
+    axios
+    .get("https://api.github.com/chelsabeth/followers")
+    .then(res => {
+      this.setState({
+        followers: res.data})
+    });
+    console.log("My Github Followers Data: ", this.state.followers);
+}
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Github UserCard App</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
